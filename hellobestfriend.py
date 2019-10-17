4889d67c164d93db66997e1f80cfab1b99a0ce79
ogolla10000 <56622305+ogolla10000@users.noreply.github.com>
2019-10-17 07:49:17 +0200

    Add files via upload

---
+++ hellobestfriend.py
@@ -0,0 +1,15 @@
+print("Hello World")
+name= input("Enter your name please: ")
+print("Hello", name)
+name= input("What is the name of your best friend?")
+print("Hello", name)
+my_variable= "John"
+print(my_variable)
+my_variable= 42
+print(my_variable)
+my_variable= True
+print(my_variable)
+# in this exercise, we have learnt about, assignment operator, variables, naming conventions and running a simple code in Python.
+# a statement in Python is basically a line or more of code
+# an expression, on the other hand, is a computation that generates a value.
+#comments start with # this is an example of a comment
