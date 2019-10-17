2290080c7869382ef256a7882832093516e587ed
ogolla10000 <56622305+ogolla10000@users.noreply.github.com>
2019-10-17 12:56:40 +0200

    exercise_chapter_4

---
+++ exercise_4.10.py
@@ -0,0 +1,9 @@
+string_1 = 'Denyse, Marie, Smith, 21, London, UK'
+print(string_1.replace(","," "))
+string_2 = input('please enter your first name: ')
+string_3 = input('please enter your surname: ')
+new_string = (string_2 + ' ' + string_3)
+print(new_string)
+print(len(new_string))
+print(new_string.upper())
+print(new_string == 'Albus')
